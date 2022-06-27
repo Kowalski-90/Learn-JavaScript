9d417605404768d83e081a6d8ffe0cabd29bf71d
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2022-06-27 15:44:57 -0300

    Update 1

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 var myName = 'Fred'; //used in the whole program
 let ourName = 'FreeCodeCamp'; //local variable
 const pi = 3.14159; //should never change
+
+//How to assign a variable
+var b = 2;
+
+//Initializing variables with assignment operator
+var a = 9;
+
